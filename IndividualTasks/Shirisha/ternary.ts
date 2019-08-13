6089be0227b44c03405920bf8bf032ca416b8479
kolliparakavyasree <kolliparakavyasree@gmail.com>
2019-08-13 09:14:42 +0530

    Add files via upload

---
+++ IndividualTasks/Shirisha/ternary.ts
@@ -0,0 +1,15 @@
+class ternary_op{
+    public a:number;
+    public b:number;
+ public condition() {
+        this.a=23;
+        this.b=9;
+//var r =(a>b)?"yes":"no";        
+console.log((this.a>this.b)?"yes":"no");
+        
+    }
+}
+    var op=new ternary_op();
+console.log(op.condition());
+console.log(op.a);
+console.log(op.b);
