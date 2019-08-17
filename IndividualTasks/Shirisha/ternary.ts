4b3a1cd688dd10f55cd50ddf84d2e2c646dc0d9a
kolliparakavyasree <kolliparakavyasree@gmail.com>
2019-08-17 16:33:06 +0530

    Update ternary.ts

--- IndividualTasks/Shirisha/ternary.ts
+++ IndividualTasks/Shirisha/ternary.ts
@@ -1,10 +1,10 @@
 class Ternary_op{
-    public a:number;
-    public b:number;
+    public A:number;
+    public B:number;
  public Condition() {
-        this.a=23;
-        this.b=9;
-console.log((this.a>this.b)?"yes":"no");        
+        this.A=23;
+        this.B=9;
+console.log((this.A>this.B)?"yes":"no");        
     }
 }
  var op=new Ternary_op();
