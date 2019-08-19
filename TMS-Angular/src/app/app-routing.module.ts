fca895a2aff8e2983243d199511f018cf3d46e72
KoviAnusha <40715126+KoviAnusha@users.noreply.github.com>
2019-08-18 20:47:01 -0700

    Update app-routing.module.ts

--- TMS-Angular/src/app/app-routing.module.ts
+++ TMS-Angular/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router'
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ErrorComponent } from './error/error.component';
 
+//if ("path"=="about"){"component":"AboutComponent"}
 
-const routes: Routes = [];
+export const myRouteConfig:Routes =[
+  //if no parameters are passed after localhost
+  {path:'',redirectTo:'products',pathMatch:'full'} //plese match full path after localhost
+  ,{path:"about",component:AboutComponent} //passing parameter to url we use :
+  ,{path:"products",component:ProductsComponent},
+  {path:"**",component:ErrorComponent}]; //any thing not equal to the above ones, we use **
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(myRouteConfig) //forRoot means for top most in the hierarchy.(appmodule.ts)
+  ],
+  exports:[
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutesModule { }
