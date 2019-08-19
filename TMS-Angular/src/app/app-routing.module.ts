8ab17a639e7c3b47889d1728cddd56cf1e793da2
KoviAnusha <40715126+KoviAnusha@users.noreply.github.com>
2019-08-18 20:53:25 -0700

    Update app-routing.module.ts

--- TMS-Angular/src/app/app-routing.module.ts
+++ TMS-Angular/src/app/app-routing.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router'
 import { MainComponent } from './main/main.component';
-
+import {WelcomeCompnent} from '/welcome/welcome.component';
 import { RegisterComponent } from './register/register.component';
-import { Tracking } from './tracking/tracking.component';
+import { TrackingComponent } from './tracking/tracking.component';
 import { LoginComponent } from './login/login.component';
 
 //if ("path"=="about"){"component":"AboutComponent"}
 
 export const myRouteConfig:Routes =[
   //if no parameters are passed after localhost
-  {path:'',redirectTo:'products',pathMatch:'full'} //plese match full path after localhost
-  ,{path:"about",component:AboutComponent} //passing parameter to url we use :
-  ,{path:"products",component:ProductsComponent},
+  {path:'',redirectTo:'welcome',pathMatch:'full'} //plese match full path after localhost
+  ,{path:"welcome",component:WelcomeComponent} //passing parameter to url we use :
+  ,{path:"register",component:RegisterComponent},
   {path:"login",component:LoginComponent},
-  {path:"**",component:ErrorComponent}]; //any thing not equal to the above ones, we use **
+  {path:"tracking",component:TrackingComponent},
+  {path:"main",component:MainComponent},
+  {path:"**",component:WelcomeComponent}]; //any thing not equal to the above ones, we use **
 
 @NgModule({
   declarations: [],
