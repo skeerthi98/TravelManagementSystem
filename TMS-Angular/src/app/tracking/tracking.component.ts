afd8b70f31014c8f32bb751f9d3f642e0746bebf
Anya-kurapati <53891834+Anya-kurapati@users.noreply.github.com>
2019-08-20 08:41:20 +0530

    Add files via upload
    json added

--- TMS-Angular/src/app/tracking/tracking.component.ts
+++ TMS-Angular/src/app/tracking/tracking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-tracking',
@@ -7,42 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient) {}
   CurrentPosition:any[];
   CurrentLocation:string[];
   CurrentTime:string[];
   Source:string;
   Destination:string;
   OnTrack:boolean=false;
+
+  response:any;
+  BusNum:any;
   ngOnInit() {
   }
     PassengerLocation:string="";
     BusCurrentLocation:string="Goa";
-   
 
-    // static getBusCurrentLocation():string {
-    //     return "gets the Current Location";
-    // }
-    // public  getPassengerLocation=():string=>{
-    //     return "gets the Current Location of passenger";
-    // }
-    // public ShowPathInBetween =()=>{
-
-    //   var BusL =()=> {
-    //         return "gets the Current Location";
-    
-    //     // BusTracking.getPassengerLocation();
-    //     // return "gets the Curren tLocation of passenger";
-    // }
-    // var PassL=()=>{
-    //         return "gets the Current Location of passenger";
-    //      }
-    // }
-    //return " "+busL+" * " + passL;
-
-// console.log(BusTracking.getBusCurrentLocation());
-// console.log(BusTracking.getPassengerLocation());
-  // console.log(showPathInBetween());
+  TrackBus(busnum:string){
+    this.BusNum=busnum;
+    this.http.get("http://localhost:3000/"+busnum)
+      .toPromise()
+      .then(data =>{this.response=data;})
+      .catch((error)=>{console.error("404:",error)})
+      ,(error=>console.error("error",error));
+  }
 
   Track(BusNum:string){
     if(BusNum == '12345'){
@@ -51,18 +39,13 @@ export class TrackingComponent implements OnInit {
       this.Source="Secunderabad";
       this.Destination="Goa";
 
-      this.CurrentPosition=[
-        {Time:"12:00", Loc:"Secunderabad"},
-        {Time:"01:25", Loc:"Hyderabad"},
-        {Time:"23:50", Loc:"Belgaum"},
-      ];
-      // this.CurrentLocation[1]="Secunderabad";
-      // this.CurrentLocation[2]="Hyderabad";
-      // this.CurrentLocation[3]="Belgaum";
-      
-      // this.CurrentTime[1]="12:00";
-      // this.CurrentTime[2]="01:25";
-      // this.CurrentTime[3]="23:50";
+      this.TrackBus("12345");
+
+      // this.CurrentPosition=[
+      //   {Time:"12:00", Loc:"Secunderabad"},
+      //   {Time:"01:25", Loc:"Hyderabad"},
+      //   {Time:"23:50", Loc:"Belgaum"},
+      // ];
       
     }
     if(BusNum == '54321'){
@@ -71,22 +54,16 @@ export class TrackingComponent implements OnInit {
       this.Source="Hyderabad";
       this.Destination="Bangalore";
 
-      this.CurrentPosition=[
-        {Time:"05:30", Loc:"Hyderabad"},
-        {Time:"20:25", Loc:"Kurnool"},
-        {Time:"21:55", Loc:"Raichur"},
-        {Time:"01:30", Loc:"Mangalore"}
-      ];
+      this.TrackBus("54321");
 
-      // this.CurrentLocation[1]="Hyderabad";
-      // this.CurrentLocation[2]="Kurnool";
-      // this.CurrentLocation[3]="Raichur";
-      // this.CurrentLocation[4]="Mangalore";
+      // this.CurrentPosition=[
+      //   {Time:"05:30", Loc:"Hyderabad"},
+      //   {Time:"20:25", Loc:"Kurnool"},
+      //   {Time:"21:55", Loc:"Raichur"},
+      //   {Time:"01:30", Loc:"Mangalore"}
+      // ];
+
       
-      // this.CurrentTime[1]="05:30";
-      // this.CurrentTime[2]="20:25";
-      // this.CurrentTime[3]="21:55";
-      // this.CurrentTime[4]="01:30";
     }
   }
   
